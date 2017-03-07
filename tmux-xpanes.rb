a1eb1b447ab9b7d35b1b393ed2c8ed352dd02e4b
greymd <greengregson@gmail.com>
2017-03-07 18:38:36 +0900

    Add formula for tmux-xpanes.

---
+++ tmux-xpanes.rb
@@ -0,0 +1,17 @@
+class TmuxXpanes < Formula
+  desc "Smart and awesome way to open on tmux panes."
+  homepage "https://github.com/greymd/tmux-xpanes"
+  url "https://github.com/greymd/tmux-xpanes.git", :revision => "4125996d5c80829d3029ad6bae2b27bbbab7ed21"
+  version "1.3.0"
+  head "https://github.com/greymd/tmux-xpanes.git", :branch => "master"
+
+  def install
+    %w[xpanes tmssh].each do |cmd|
+      bin.install "bin/#{cmd}", "bin/#{cmd}"
+    end
+  end
+
+  test do
+    system "false"
+  end
+end
