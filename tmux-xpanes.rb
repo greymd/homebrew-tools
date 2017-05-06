557052452cbea90919c5d50b0f56cba7789f44ef
greymd <greengregson@gmail.com>
2017-05-07 00:09:37 +0900

    Add dependency with tmux

--- tmux-xpanes.rb
+++ tmux-xpanes.rb
@@ -4,9 +4,10 @@ class TmuxXpanes < Formula
   url "https://github.com/greymd/tmux-xpanes.git", :revision => "4125996d5c80829d3029ad6bae2b27bbbab7ed21"
   version "1.3.0"
   head "https://github.com/greymd/tmux-xpanes.git", :branch => "master"
+  depends_on "tmux"
 
   def install
-    %w[xpanes tmssh].each do |cmd|
+    %w[xpanes tmux-xpanes].each do |cmd|
       bin.install "bin/#{cmd}"
     end
   end
