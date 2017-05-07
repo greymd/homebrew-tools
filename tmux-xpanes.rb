8b566c87809a39f39e350647c88d2c8238fb6a88
greymd <greengregson@gmail.com>
2017-05-08 01:56:43 +0900

    Upgrade version to 2.0.0

--- tmux-xpanes.rb
+++ tmux-xpanes.rb
@@ -1,8 +1,8 @@
 class TmuxXpanes < Formula
   desc "Smart and awesome way to open on tmux panes."
   homepage "https://github.com/greymd/tmux-xpanes"
-  url "https://github.com/greymd/tmux-xpanes.git", :revision => "4125996d5c80829d3029ad6bae2b27bbbab7ed21"
-  version "1.3.0"
+  url "https://github.com/greymd/tmux-xpanes.git", :revision => "f9187f14e9e6a5be1d43965ab954d5b0da377776"
+  version "2.0.0"
   head "https://github.com/greymd/tmux-xpanes.git", :branch => "master"
   depends_on "tmux"
 
