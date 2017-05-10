63e7c024beda81113e4e7efbb1a081613a9df235
greymd <greengregson@gmail.com>
2017-05-10 19:07:56 +0900

    tmux-xpanes: Change description.

--- tmux-xpanes.rb
+++ tmux-xpanes.rb
@@ -1,5 +1,5 @@
 class TmuxXpanes < Formula
-  desc "Smart and awesome way to open on tmux panes."
+  desc "Ultimate terminal divider powered by tmux."
   homepage "https://github.com/greymd/tmux-xpanes"
   url "https://github.com/greymd/tmux-xpanes.git", :revision => "41dd1873e273c7a2fabacaa5d08902f792b06d95"
   version "2.0.1"
