e616e4d05b761adbc3480b3564b1ce8c6ccf0dd9
greymd <greengregson@gmail.com>
2017-11-15 22:34:12 +0900

    tmux-xpanes: Bump version to v2.2.1

--- tmux-xpanes.rb
+++ tmux-xpanes.rb
@@ -1,10 +1,10 @@
 class TmuxXpanes < Formula
-  desc "Ultimate terminal divider powered by tmux."
-  homepage "https://github.com/greymd/tmux-xpanes"
-  url "https://github.com/greymd/tmux-xpanes.git", :revision => "d5fb7c928e90dbfa33159469e3719f9d8632c72a"
-  version "2.2.0"
-  head "https://github.com/greymd/tmux-xpanes.git", :branch => "master"
-  depends_on "tmux"  => :recommended
+  desc 'Ultimate terminal divider powered by tmux'
+  homepage 'https://github.com/greymd/tmux-xpanes'
+  url 'https://github.com/greymd/tmux-xpanes.git', :revision => 'v2.2.1'
+  version '2.2.0'
+  head 'https://github.com/greymd/tmux-xpanes.git', :branch => 'master'
+  depends_on 'tmux' => :recommended
 
   def install
     %w[xpanes tmux-xpanes].each do |cmd|
@@ -14,6 +14,6 @@ class TmuxXpanes < Formula
   end
 
   test do
-    system "false"
+    system 'false'
   end
 end
