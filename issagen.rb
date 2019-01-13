bda20d8211c0c0f9ca4dedf2f3f0f94ac581cb61
greymd <greengregson@gmail.com>
2019-01-13 17:55:12 +0000

    Add issagen

---
+++ issagen.rb
@@ -0,0 +1,18 @@
+class Issagen < Formula
+  desc "Issagen: Issa generator"
+  homepage "https://github.com/greymd/issagen"
+  url "https://github.com/greymd/issagen/archive/v1.0.0.tar.gz"
+
+  bottle :unneeded
+
+  depends_on "imagemagick" => :recommended
+
+  def install
+    system "./install.sh", prefix
+  end
+
+  test do
+    # Check options with valid combination
+    pipe_output("#{bin}/issagen --version", "v1.0.0", 0)
+  end
+end
