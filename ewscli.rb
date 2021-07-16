65f0aad7d3eb6956e9379e77f97770a1a0ec0779
greymd <yamada@gr3.ie>
2021-07-16 01:08:56 +0100

    ewscli v1.0.6

--- ewscli.rb
+++ ewscli.rb
@@ -1,9 +1,9 @@
 class Ewscli < Formula
-  version "1.0.4"
+  version "1.0.6"
   desc "Exchange Web Service CLI"
   homepage "https://github.com/greymd/ewscli"
   url "https://github.com/greymd/ewscli/releases/download/v#{version}/ewscli-#{version}-x86_64-darwin.tar.gz"
-  sha256 "494bbf9b515bbf361a1730728326ca380a75e79170fac1c9e665034661fd409d"
+  sha256 "06bd22713d2bc4b1ce195c68fd3c20815a3db3f923613d5d1ec286861314411b"
 
   bottle :unneeded
 
