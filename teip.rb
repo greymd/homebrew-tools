80308dea449eca34c2c960534ee48ba4bf1df1e7
Yasuhiro Yamada <yamada@gr3.ie>
2023-03-07 03:49:15 +0000

    teip: v2.2.0

--- teip.rb
+++ teip.rb
@@ -1,14 +1,23 @@
 class Teip < Formula
-  version "2.1.0"
-  desc "Highly efficient \"Masking tape\" for standard input"
+  version "2.2.0"
+  desc "Masking tape to help commands \"do one thing well\""
   homepage "https://github.com/greymd/teip"
-  url "https://github.com/greymd/teip/releases/download/v#{version}/teip-#{version}.x86_64-apple-darwin.tar.gz"
-  sha256 "fab67b1b614b28305eefbacee76e2b120882e7a0c1762b52d12e827565667573"
+
+  # 64-bit ARM version for Apple Silicon
+  if Hardware::CPU.arm?
+    url "https://github.com/greymd/teip/releases/download/v#{version}/teip-#{version}.aarch64-apple-darwin.tar.gz"
+    sha256 "e403b493daf6a97a2ef91df444ba4a292f96f1d4764c3e667e6710c4fbeb321c"
+  else
+    url "https://github.com/greymd/teip/releases/download/v#{version}/teip-#{version}.x86_64-apple-darwin.tar.gz"
+    sha256 "ef041adf0699cd4ba05b4ba76de2ea94847fbd8c12f9b83142ae904f8f551a81"
+  end
 
   def install
     bin.install "bin/teip"
     man1.install "man/teip.1"
     zsh_completion.install "completion/zsh/_teip"
+    fish_completion.install "completion/fish/teip.fish"
+    bash_completion.install "completion/bash/teip"
   end
 
   test do
